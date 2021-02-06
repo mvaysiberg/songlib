b9dcac2a1890016061458d6d20a6246b5d42af50
David Wang <davidwangmo@gmail.com>
2021-02-05 20:53:52 -0600

    first commit

---
+++ src/songlib/SongLib.java
@@ -0,0 +1,12 @@
+package songlib;
+
+import javafx.application.Application;
+
+public class SongLib extends Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
