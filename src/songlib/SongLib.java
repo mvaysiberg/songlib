bdff9579256aa8b38361468a657c00302a353c69
David Wang <davidwangmo@gmail.com>
2021-02-05 21:05:34 -0600

    import stage

--- src/songlib/SongLib.java
+++ src/songlib/SongLib.java
@@ -1,12 +1,18 @@
 package songlib;
 
 import javafx.application.Application;
+import javafx.stage.Stage;
 
 public class SongLib extends Application {
 
+	public void start(Stage primaryStage) {
+		
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		launch(args);
 	}
 
 }
