606044a1c8e7976c2a44f5ec5e65526f8c2ffc81
mvaysiberg <mvays11@gmail.com>
2021-02-06 22:02:19 -0500

    selected list listener (not working)

--- src/view/ListController.java
+++ src/view/ListController.java
@@ -6,16 +6,20 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 public class ListController {
 	@FXML Button add;
 	@FXML Button edit;
 	@FXML Button delete;
-	
+	@FXML Text name;
+	@FXML Text artist;
+	@FXML Text album;
+	@FXML Text year;
 	@FXML ListView<String> listView;
 	private ObservableList<String> obsList;
 	public void start() {
-		obsList = FXCollections.observableArrayList();
+		obsList = FXCollections.observableArrayList("test1", "test2");
 		//get data from csv
 		listView.setItems(obsList);
 	}
@@ -32,5 +36,15 @@ public class ListController {
 	public void start(Stage mainstage) {
 		//select the first item
 		//listView.getSelectionModel().select(0);
+		
+		listView.getSelectionModel().selectedIndexProperty().
+		addListener((obs, oldVal, newVal)-> showItem(mainstage));
+	}
+	
+	private void showItem(Stage mainstage) {
+		//System.out.println("test");
+		int index = listView.getSelectionModel().getSelectedIndex();
+		String content = listView.getSelectionModel().getSelectedItem();
+		System.out.println("index: " + index + " content: " + content);
 	}
 }
